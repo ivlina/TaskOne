49beb04930d4fb3e6e9633f9e1ccae42ed2d54ab
“Ivlina <ivlinaross@gmail.com>
2021-05-29 11:02:05 +0300

    Initial Commit

---
+++ TaskOne/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TaskOne
+//
+//  Created by Ivlina Moroshkina on 29/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
